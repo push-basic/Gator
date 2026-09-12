4a7ee476f11ac4373e7a3c9b521ec042e08186da
push-basic <staltpriv@proton.me>
2026-09-12 18:41:59 +0200

    sqlOpen in main

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/lib/pq"
 	"github.com/push-basic/Gator/internal/config"
+	"github.com/push-basic/Gator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -18,8 +22,17 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
 	programState := &state{
 		cfg: &cfg,
+		db:  dbQueries,
 	}
 
 	cmds := commands{

1a94832427b10570e0b33656881bafb7afcdec26
push-basic <staltpriv@proton.me>
2026-09-12 14:31:00 +0200

    commands, config main

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -15,9 +16,30 @@ type Config struct {
 	Name string `json:"current_user_name"`
 }
 
-func (c *Config) SetUser(param string) error {
-	c.Name = param
-	return writeConfig(*c)
+func Read() (Config, error) {
+	cfgPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, fmt.Errorf("getting config path: %w", err)
+	}
+
+	jsonData, err := os.ReadFile(cfgPath)
+	if err != nil {
+		return Config{}, fmt.Errorf("reading config file: %w", err)
+	}
+
+	var config Config
+
+	err = json.Unmarshal(jsonData, &config)
+	if err != nil {
+		return Config{}, fmt.Errorf("unmarshalling json: %w", err)
+	}
+
+	return config, nil
+}
+
+func (cfg *Config) SetUser(param string) error {
+	cfg.Name = param
+	return writeConfig(*cfg)
 }
 
 func getConfigFilePath() (string, error) {
@@ -27,4 +49,23 @@ func getConfigFilePath() (string, error) {
 	}
 	configPath := filepath.Join(homeDir, configFileName)
 	return configPath, nil
+}
+
+func writeConfig(cfg Config) error {
+	cfgPath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("getting config path: %w", err)
+	}
+
+	jsonData, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return fmt.Errorf("mashalling config: %w", err)
+	}
+
+	err = os.WriteFile(cfgPath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("writing config file: %w", err)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -4,24 +4,37 @@ import (
 	"Gator/internal/config"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	cfg, err := config.ReadConfig()
+	cfg, err := config.Read()
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
 
-	err = cfg.SetUser("push")
+	s := &state{
+		Config: &cfg,
+	}
+
+	cmds := newCommands()
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
+		os.Exit(1)
+
+	}
+
+	cmd := command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	err = cmds.run(s, cmd)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-
-	cfg, err = config.ReadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("URL:", cfg.URL)
-	fmt.Println("Name:", cfg.Name)
 }

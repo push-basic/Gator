044bc1c9a17c1bb9998b7d74ee615f5b9c825a4b
push-basic <staltpriv@proton.me>
2026-09-14 17:11:00 +0200

    addfeed

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerGetUsers)
 	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", addFeedHandler)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
